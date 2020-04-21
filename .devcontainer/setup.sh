ab2ea65d0ef6987aae60c50328d328663703426d
Daniel Katz <daniekatz@gmail.com>
2020-04-21 11:50:43 +0000

    Wait for entrypoint script to finish

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,13 @@
 workspace="/workspace"
 symlink_target="/var/www/html/wp-content/plugins/test-plugin"
 
+# To avoid interfering with WordPress initialization, wait for it to finish
+until pids=$(pidof php-fpm)
+do
+  echo 'Waiting for initialization to finish...'
+  sleep 1
+done
+
 # Create a symlink if not already exists
 if [ ! -L "$symlink_target" ]; then
   echo "Creating symlink from '${workspace}' to '${symlink_target}'..."
