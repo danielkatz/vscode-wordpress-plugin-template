d81d46fe0ed8c5e1e2e4bb4348926ca42ba4edc0
Daniel Katz <daniekatz@gmail.com>
2020-04-21 10:51:14 +0000

    Add REAMDE

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,7 +4,7 @@ workspace="/workspace"
 symlink_target="/var/www/html/wp-content/plugins/test-plugin"
 
 # Create a symlink if not already exists
-if [ ! -d "$symlink_target" ]; then
+if [ ! -L "$symlink_target" ]; then
   echo "Creating symlink from '${workspace}' to '${symlink_target}'..."
   ln -s "$workspace" "$symlink_target"
 fi
