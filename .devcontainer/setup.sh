3e15f7f153f85bd4ab915ea8d74c5a9df4ba5a5d
Daniel Katz <daniekatz@gmail.com>
2020-04-21 09:40:39 +0000

    Refactor symlink creation process

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+workspace="/workspace"
+symlink_target="/var/www/html/wp-content/plugins/test-plugin"
+
+# Create a symlink if not already exists
+if [ ! -d "$symlink_target" ]; then
+  echo "Creating symlink from '${workspace}' to '${symlink_target}'..."
+  ln -s "$workspace" "$symlink_target"
+fi
