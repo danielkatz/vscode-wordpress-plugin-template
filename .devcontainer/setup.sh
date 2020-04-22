374fda418b751b55860ab2929e26bc5e75e70ece
Daniel Katz <daniekatz@gmail.com>
2020-04-22 16:30:06 +0000

    Create symlinks in a background process

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-workspace='/workspace'
-symlink_target='/var/www/html/wp-content/plugins/dev-plugin'
+echo "Running symlink-setup in background"
 
-# To avoid interfering with WordPress initialization, wait for it to finish
-until pids=$(pidof php-fpm)
-do
-  echo 'Waiting for initialization to finish...'
-  sleep 1
-done
-
-# Create a symlink if not already exists
-if [ ! -L "$symlink_target" ]; then
-  echo "Creating symlink from '${workspace}' to '${symlink_target}'..."
-  ln -s "$workspace" "$symlink_target"
-fi
+nohup ./.devcontainer/setup-symlink.sh > /dev/null 2> /dev/null < /dev/null &
